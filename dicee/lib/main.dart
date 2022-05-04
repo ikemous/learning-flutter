b533bce3f8527aa830bccbb347f481c652bd6a6a
Ikemous <barranco.ike@outlook.com>
2022-05-04 10:17:23 -0600

    finished dicee app

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,11 +17,100 @@ void main() {
   );
 }
 
-class DiceeApp extends StatelessWidget {
-  const DiceeApp({Key? key}) : super(key: key);
+class DiceeApp extends StatefulWidget {
+  const DiceeApp({super.key});
+
+  @override
+  State<DiceeApp> createState() => _DiceeAppState();
+}
+
+class _DiceeAppState extends State<DiceeApp> {
+  int leftDiceNumber = Random().nextInt(6) + 1;
+  int rightDiceNumber = Random().nextInt(6) + 1;
+
+  void rollDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () => rollDice(),
+              child: Image(
+                image: AssetImage('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () => rollDice(),
+              child: Image(
+                image: AssetImage('images/dice$rightDiceNumber.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
+// class DiceeApp extends StatelessWidget {
+//   const DiceeApp({Key? key}) : super(key: key);
+
+//   final leftDiceNumber = 1;
+//   final rightDiceNumber = 2;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Row(
+//         children: [
+//           Expanded(
+//             child: TextButton(
+//               onPressed: () {
+//                 dev.log("Left Drice Clicked");
+//               },
+//               child: Image(
+//                 image: AssetImage('images/dice$leftDiceNumber.png'),
+//               ),
+//             ),
+//           ),
+//           Expanded(
+//             child: TextButton(
+//               onPressed: () {
+//                 dev.log("Right Dice Clicked");
+//               },
+//               child: Image(
+//                 image: AssetImage('images/dice$rightDiceNumber.png'),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+
+// Row(
+//       children: const [
+//         Expanded(
+//           child: Image(
+//             image: AssetImage('images/dice1.png'),
+//           ),
+//         ),
+//         Expanded(
+//           child: Image(
+//             image: AssetImage('images/dice1.png'),
+//           ),
+//         ),
+//       ],
+//     );
