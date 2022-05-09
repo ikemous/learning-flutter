ed41737cecdc2bfd6215b03e05d524751ba997a4
Ikemous <barranco.ike@outlook.com>
2022-05-09 13:06:45 -0600

    added initial files

---
+++ weather/lib/services/weather.dart
@@ -0,0 +1,49 @@
+class WeatherModel {
+  String getWeatherIcon(int condition) => condition < 300
+      ? '🌩'
+      : condition < 400
+          ? '🌧'
+          : condition < 600
+              ? '☔️'
+              : condition < 700
+                  ? '☃️'
+                  : condition < 800
+                      ? '🌫'
+                      : condition == 800
+                          ? '☀️'
+                          : condition <= 804
+                              ? '☁️'
+                              : '🤷‍';
+
+  // String getWeatherIcon(int condition) {
+  //   if (condition < 300) {
+  //     return '🌩';
+  //   } else if (condition < 400) {
+  //     return '🌧';
+  //   } else if (condition < 600) {
+  //     return '☔️';
+  //   } else if (condition < 700) {
+  //     return '☃️';
+  //   } else if (condition < 800) {
+  //     return '🌫';
+  //   } else if (condition == 800) {
+  //     return '☀️';
+  //   } else if (condition <= 804) {
+  //     return '☁️';
+  //   } else {
+  //     return '🤷‍';
+  //   }
+  // }
+
+  String getMessage(int temp) {
+    if (temp > 25) {
+      return 'It\'s 🍦 time';
+    } else if (temp > 20) {
+      return 'Time for shorts and 👕';
+    } else if (temp < 10) {
+      return 'You\'ll need 🧣 and 🧤';
+    } else {
+      return 'Bring a 🧥 just in case';
+    }
+  }
+}
