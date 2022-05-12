9faf0ee15eb7270ed0a8c70272689f59502a21c4
Ikemous <barranco.ike@outlook.com>
2022-05-12 10:30:20 -0600

    added initial files

---
+++ bitcoin_ticker/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:bitcoin_ticker/price_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Colors.lightBlue,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.lightBlue,
+        ),
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const PriceScreen(),
+    );
+  }
+}
